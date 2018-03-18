1c2689cc0a317387b399d06dfb6350899eed0433
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-03-18 04:51:18 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarfs)# code an argument here
+  dwarfs.each_with_index{|dwarf| puts dwarf}# Your code here
 end
 
 def summon_captain_planet# code an argument here
