d9bb441e4ed88c1874a19079b37f07682651e82c
Jeremiah Lutes <jeremiahlutes@gmail.com>
2018-03-18 05:14:00 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(planeteer)# code an argument here
   planeteer.collect{|call| "#{call.capitalize}!"}#  code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(words)# code an argument here
+  words.include? {|word| word.length > 4}# Your code here
 end
 
 def find_the_cheese# code an argument here
